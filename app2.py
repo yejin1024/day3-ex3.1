164e397fd798cb8e96a5fdca485193880a014b19
yejin1024 <yejin1024@gmail.com>
2025-10-30 15:32:21 +0900

    Update app2.py
    3.6.9게임과 동일하게 3.6.9갯수만큼 짝이 출력되야 하므로
    es)98 , 97 -> 짝 으로
    99, 66->은 짝짝으로 출력되도록 수정하였습니다

--- app2.py
+++ app2.py
@@ -1,9 +1,14 @@
-
+<<<<<<< HEAD
 for i in range(100, 0, -1):  # 100에서 1까지 역순 출력
     num_str = str(i)
     # 숫자 안에 포함된 3, 6, 9의 개수를 모두 합산
     clap_count = sum(num_str.count(d) for d in ['3', '6', '9'])
 
+=======
+for i in range(100, 0, -1):
+    num_str = str(i)
+    clap_count = num_str.count('3')
+>>>>>>> 3d8df2a25308787a0653933c1374f2701d642649
     if clap_count > 0:
         print("짝" * clap_count)
     else:
